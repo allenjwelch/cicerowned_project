03d61afe707213c95e9ce8341ed4c939bd084345
Allen Welch <allenjwelch@gmail.com>
2018-07-07 19:34:25 -0400

    flashcard working with hard code from flashcard page

--- client/src/components/FlashCards/FlashCards.js
+++ client/src/components/FlashCards/FlashCards.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { Col, Button } from 'react-materialize';
 import "./FlashCards.css";
 
-//TODO - Add ternary statement and setState of 'flipped: true/false' to toggle classes'
-
+let cardArray; 
 
 class FlashCards extends Component {
 
   state = {
     flipped: false,
+    cardPos: 0,
+    activeDeck: this.props,
   }
 
+
   componentDidMount() {
-    
+    this.objectToArray(); 
+      // .then(
+      //   console.log(cardArray));
+      //   this.setState({ activeDeck : cardArray })
+      // .catch(err => console.log(err));
+  };
+
+  objectToArray = () => {
+    let obj = this.props; 
+    // console.log(obj); 
+    cardArray = Object.keys(obj).map(function(key) {
+      return obj[key];
+    });
+    this.setState({ activeDeck : cardArray }) //! Needs either callback or promise to run sync
+    return cardArray; 
   }
-  
+
   flip = () => {
     if (this.state.flipped === false) {
       this.setState({ flipped: true });
@@ -23,56 +39,81 @@ class FlashCards extends Component {
     }
   };
 
+  changeCard = () => {
+    // move user to next card in deck ====
+    this.setState({ cardPos: this.state.cardPos + 1 });
+    console.log('card Pos', this.state.cardPos); 
+    console.log('deck length', this.state.activeDeck.length); 
+    console.log(this.state.activeDeck); 
+
+    if (this.state.cardPos > this.state.activeDeck.length - 2) { //! Changed length of deck check
+      this.setState({ cardPos: 0 });
+      console.log(this.state.cardPos); 
+    }
+    return this.state.cardPos; 
+  }
+
+  nailedIt = () => {
+    //! deletes too many cards if first failedit
+    let cardCount = this.countInArray(this.state.activeDeck, this.state.activeDeck[this.state.cardPos]); 
+    console.log('cardCount', cardCount)
+    if (cardCount > 1) {
+      this.state.activeDeck.shift(); 
+    }
+    this.changeCard(); 
+  }
+
+  failedIt = () => {
+    this.state.activeDeck.splice(Math.ceil(this.state.activeDeck.length / 2), 0, this.state.activeDeck[this.state.cardPos])
+    this.changeCard(); 
+  }
+
+  // function to count cards in array
+  countInArray(array, what) {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === what) {
+        count++; 
+      }
+    }
+    return count; 
+  }
+
   // constructor(props) {
   //   super(props);
-  // }
-
-
-  // flip() {
-  //   if (this.setState === false) {
-  //     this.setState({ flipped: true });
-  //   } else {
-  //     this.setState({ flipped: false });
-  //   }
-
-    // let card = this.card 
-    // alert(this.card); 
-    // console.log(card); 
-    // card.classList.toggle("flipped");
-    // let card = ReactDOM.findDOMNode(this.refs.card);
-    // card.classList.toggle('flipped');
-  // }
+  //   this.state.activeDeck = this.props; 
+  //   cardPos = 0; 
+  // };
 
 
   render() {
     return (
       <div className="flashCard" >
-        <h3>Flash Cards Component</h3>
-        <h4>{this.props.test}</h4>
-
-        <section class="container">
+        <Col s={12} m={10} l={8} xl={8} className='container'>
+        {/* <section class="container"> */}
           <div 
             onClick={this.flip}
             className={
               this.state.flipped === false ? "card" : "card flipped"
             }>
             <div className="front">
-              <h1>familyName</h1>
-              <h3>Style: styleName</h3>
+              <h1>{this.state.activeDeck[this.state.cardPos].familyName}</h1>
+              <h3>Style: {this.state.activeDeck[this.state.cardPos].styleName}</h3>
             </div>
             <div className="back">
-              <img src="image" alt="beer style image"/>
-              <p>ABV: "#"</p>
-              <p>IBUs: "#"</p>
-              <p>SRM: "#"</p>
-              <p>briefDescription: "#"</p>
-              <p>examples</p>
-              <a class="cardBtns" id="nailed" href="#">Nailed it!</a>
-              <a class="cardBtns" id="almost" href="#">Almost</a>
-              <a class="cardBtns" id="failed" href="#">Failed it</a>
+              <img src={this.state.activeDeck[this.state.cardPos].image} alt="beer style image"/>
+              <p>ABV: {this.state.activeDeck[this.state.cardPos].ABV}</p>
+              <p>IBUs: {this.state.activeDeck[this.state.cardPos].IBU}</p>
+              <p>SRM: {this.state.activeDeck[this.state.cardPos].SRM}</p>
+              <p>Description: {this.state.activeDeck[this.state.cardPos].briefDescription}</p>
+              <p>{this.state.activeDeck[this.state.cardPos].example.join(', ')}</p>
+              <a className="cardBtns" id="nailed" onClick={this.nailedIt}>Nailed it!</a>
+              <a className="cardBtns" id="almost" onClick={this.changeCard}>Almost...</a>
+              <a className="cardBtns" id="failed" onClick={this.failedIt}>Failed it</a>
             </div>
           </div>
-        </section>
+         {/* </section> */}
+        </Col>
       </div>
     );
   }
--- client/src/pages/FlashCard/FlashCard.js
+++ client/src/pages/FlashCard/FlashCard.js
@@ -8,16 +8,60 @@ import "./FlashCard.css";
 // import API from "../../utils/API"; 
 
 
+
 class FlashCard extends Component {
   state = {
+    //! Hard coded user decks
     userDecks: [], 
-    activeDecks: [], 
-    test: "Test State"
+    activeDeck: [
+      {
+        styleName: 'style1', 
+        familyName: 'familyName',
+        ABV: 5.5, 
+        IBU: 10, 
+        BU: 5.5, 
+        SRM: 20, 
+        image: '#',
+        description: 'Sample', 
+        example: [
+          'example 1', 
+          'example 2'
+        ]
+      }, {
+        styleName: 'style2', 
+        familyName: 'familyName',
+        ABV: 5.5, 
+        IBU: 10, 
+        BU: 5.5, 
+        SRM: 20, 
+        image: '#',
+        description: 'Sample', 
+        example: [
+          'example 1', 
+          'example 2'
+        ]
+      }, {
+        styleName: 'style3', 
+        familyName: 'familyName',
+        ABV: 5.5, 
+        IBU: 10, 
+        BU: 5.5, 
+        SRM: 20, 
+        image: '#',
+        description: 'Sample', 
+        example: [
+          'example 1', 
+          'example 2'
+        ]
+      },
+    ], 
   }
 
   // componentDidMount() {
   //   this.loadDecks();
   // }
+
+  //TODO function for adding userDecks to Active Deck (should contain only 1 deck at a time)
 
   // loadDecks = () => {
   //   API.getDecks()
@@ -34,7 +78,7 @@ class FlashCard extends Component {
         <Row>
           <Col s={12} m={8} l={8} xl={8} className='col1'>
 
-            <FlashCards {...this.state} />
+            <FlashCards {...this.state.activeDeck} />
 
           </Col>
           <Col s={12} m={4} l={4} xl={4} className='col2'>
@@ -43,9 +87,13 @@ class FlashCard extends Component {
               <hr />
               <CollapsibleItem header='Beer Styles' icon='filter_drama'>
                 List of user's beer style decks here.
+                //TODO add all userDecks of 'Beer Styles' into lists
+          
               </CollapsibleItem>
               <CollapsibleItem header='Public Decks' icon='place'>
                 List of user's public decks here.
+                //TODO add all userDecks of 'Public' type into lists
+
               </CollapsibleItem>
             </Collapsible>
           </Col>
