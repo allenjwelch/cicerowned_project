0244a55d125e72c5b3f98c4db5273f4df5d40bba
SusanLDavenport <susanldavenport@gmail.com>
2018-07-10 17:50:48 -0400

    updated user profile cards

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react"; 
-import { Row, Col, Container, Card } from "react-materialize";
+import { Row, Col, Card } from "react-materialize";
+import './style.css';
 // import Auth from '../../components/Auth/Auth.js';
 
 // const auth = new Auth();
@@ -9,23 +10,41 @@ const Home = () => (
     <div container = "mainBody">
       <Row>
         <Col m={6} s={12}>
-        <Card className='blue-grey darken-1 center-align' textClassName='white-text' title={<i className="medium material-icons">book</i>} actions={[<a href='#'>This is a link</a>]}><h3>Study</h3><p>Learn the basics using our pre-made study guides.</p>
-        </Card>
+          <Card 
+          className='amber darken-1 center-align' 
+          textClassName='white-text' 
+          title={<i className="icon-orange medium material-icons">book</i>} 
+          actions={[<a href='#'>This is a link</a>]}>
+          <h3>Study</h3><p>Learn the basics using our pre-made study guides.</p>
+          </Card>
         </Col>
 
         <Col m={6} s={12}>
-        <Card className= 'blue-grey darken-1 center-align' textClassName='white-text' title={<i className="medium material-icons">book</i>} actions={[<a href='#'>This is a link</a>]}><h3>Create</h3><p>Make your own custom study guides.</p>
-        </Card>
+          <Card 
+          className= 'amber darken-1 center-align' 
+          textClassName='white-text' 
+          title={<i className="icon-orange medium material-icons">select_all</i>} 
+          actions={[<a href='#'>This is a link</a>]}>
+          <h3>Create</h3><p>Make your own custom study guides.</p>
+          </Card>
         </Col>
       
         <Col m={6} s={12}>
-        <Card className='blue-grey darken-1 center-align' textClassName='white-text' title={<i className="medium material-icons">book</i>} actions={[<a href='#'>This is a link</a>]}><h3>Study</h3><p>Learn the basics using our pre-made study guides</p>
-        </Card>
+          <Card 
+          className='amber darken-1 center-align' 
+          textClassName='white-text' title={<i className="icon-orange medium material-icons">loyalty</i>} 
+          actions={[<a href='#'>This is a link</a>]}>
+          <h3>Share</h3><p>Share study guides and learn from others by publishing custom cards.</p>
+          </Card>
         </Col>
 
         <Col m={6} s={12}>
-        <Card className='blue-grey darken-1 center-align' textClassName='white-text' title={<i className="medium material-icons">book</i>} actions={[<a href='#'>This is a link</a>]}><h3>Study</h3><p>Learn the basics using our pre-made study guides</p>
-        </Card>
+          <Card 
+          className='amber darken-1 center-align' 
+          textClassName='white-text' title={<i className="icon-orange medium material-icons">book</i>} 
+          actions={[<a href='#'>This is a link</a>]}>
+          <h3>Study</h3><p>Learn the basics using our pre-made study guides</p>
+          </Card>
         </Col>
       </Row>
     </div>
