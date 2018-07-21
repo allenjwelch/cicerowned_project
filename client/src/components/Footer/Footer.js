8e5b7139520705c577fea2e65f0af530ba07f4b6
Allen Welch <allenjwelch@gmail.com>
2018-07-21 10:56:16 -0400

    resolved merge conflict

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import {Footer, Modal, Input, Button} from 'react-materialize';
 import * as emailjs from 'emailjs-com';
 
-<<<<<<< HEAD
 class FooterComp extends Component {
   
   handleSend = event => {
@@ -71,26 +70,5 @@ class FooterComp extends Component {
     );
   };
 };  
-=======
-const FooterComp = () => (
-  <Footer copyrights="Copyright 2018 | Allen Welch, Eddie Rodriguez, + Susan Davenport"
-  moreLinks={
-    <a className="grey-text text-lighten-4 right" href="#!">GitHub</a>
-  }
-  links={
-    <ul>
-      <li><a className="grey-text text-lighten-3" href="#!">Study</a></li>
-      <li><a className="grey-text text-lighten-3" href="#!">Create</a></li>
-      <li><a className="grey-text text-lighten-3" href="#!">Chat</a></li>
-      <li><a className="grey-text text-lighten-3" href="#!">User Dashboard</a></li>
-    </ul>
-  }
-  className='amber darken-2 page-footer'
->
-    <h5 className="white-text">Contact Us</h5>
-    <p className="grey-text text-lighten-4">Interested in using Cicerowned for your bar, brewery, or restaurant? Let's talk!</p>
-</Footer>
-); 
->>>>>>> master
 
 export default FooterComp; 
